b970a031cbafeafa6c4e7437772b20b4c4c12759
lobosz <di.loboso@duocuc.cl>
2026-04-27 11:55:00 -0400

    chore: Inicialización base del proyecto Equipo B - Repuestos

---
+++ src/main/java/cl/Repuestos/EquipoB/EquipoBApplication.java
@@ -0,0 +1,13 @@
+package cl.Repuestos.EquipoB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipoBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/Repuestos/EquipoB/EquipoBApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.Repuestos.EquipoB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
